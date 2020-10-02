9913efee50d8baa51eb32385c6b4a27a9690bcdc
Adrian Gomez <aegomez@tekmexico.com>
2020-10-02 11:23:08 -0700

    Update tests for problem 17

--- 17-isPalindrome/isPalindrome.test.js
+++ 17-isPalindrome/isPalindrome.test.js
@@ -2,8 +2,15 @@ const { LinkedList } = require('./LinkedList');
 const { isPalindrome } = require('./isPalindrome');
 
 describe('isPalindrome', () => {
-  test('Detects that an input is a palindrome', () => {
+  test('Detects a palindrome with odd length', () => {
     const input = new LinkedList(...'kayak');
+    const output = isPalindrome(input);
+
+    expect(output).toBe(true);
+  });
+
+  test('Detects a palindrome with even length', () => {
+    const input = new LinkedList(1, 2, 3, 3, 2, 1);
     const output = isPalindrome(input);
 
     expect(output).toBe(true);
@@ -16,48 +23,40 @@ describe('isPalindrome', () => {
     expect(output).toBe(false);
   });
 
-  test('Punctuation, capitalization and spaces are ignored', () => {
-    const input = new LinkedList(...'Was it a car or a CAT I saw?');
-    const output = isPalindrome(input);
-
-    expect(output).toBe(true);
-  });
-
-  test('Can detect word-unit palindromes', () => {
-    // each node must contain a whole word
+  test('Can detect palindromes of different data types', () => {
     const input = new LinkedList(
-      'Did',
-      'I',
-      'say',
-      'you',
       'never',
       'say',
-      '"never',
+      'never',
+      null,
+      555,
+      555,
+      null,
+      'never',
       'say',
-      'never"?',
-      'You',
-      'say',
-      'I',
-      'did.'
+      'never'
     );
     const output = isPalindrome(input);
 
     expect(output).toBe(true);
   });
 
-  test('A single node with data is a palindrome', () => {
-    const input = new LinkedList('1');
-    const output = isPalindrome(input);
+  test('A single node with any data is a palindrome', () => {
+    const input1 = new LinkedList('BIG DATA');
+    const input2 = new LinkedList(null);
+    const input3 = new LinkedList();
 
-    expect(output).toBe(true);
+    expect(isPalindrome(input1)).toBe(true);
+    expect(isPalindrome(input2)).toBe(true);
+    expect(isPalindrome(input3)).toBe(true);
   });
 
-  test('A single node with NO data is NOT a palindrome', () => {
-    const input1 = new LinkedList(null);
-    const input2 = new LinkedList();
+  test('Detects palindromes in two-nodes lists', () => {
+    const input1 = new LinkedList('1', '11');
+    const input2 = new LinkedList('one', 'one');
 
     expect(isPalindrome(input1)).toBe(false);
-    expect(isPalindrome(input2)).toBe(false);
+    expect(isPalindrome(input2)).toBe(true);
   });
 
   test('An empty list or a non-linked list are not palindromes', () => {
